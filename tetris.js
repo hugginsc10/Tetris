5b839ea06698633c808a93f4a9e570b9078ccb9c
Chas Huggins <hugginsc10@gmail.com>
2019-10-16 14:58:26 -0400

    added background

--- tetris.js
+++ tetris.js
@@ -15,7 +15,7 @@ function createMatrix(width, height) {
 
 function createTetris() {
    let rowCount = 1;
-  outer: for (let y = arena.length -1; y > 0; y--)  {
+    outer: for (let y = arena.length -1; y > 0; y--)  {
     for (let x = 0; x < arena[y].length; x++) {
       if (arena[y][x] === 0) {
         continue outer;
